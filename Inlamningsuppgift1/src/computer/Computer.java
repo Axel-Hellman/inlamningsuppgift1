5c52b616d8221dfb791787eae6f8f3fbdee3a321
JoakimRudberg <rudberg.joakim@gmail.com>
2018-09-14 16:43:13 +0200

    Add Semicolons. Git funkar i eclipse nu men hittar inte heller readME.

--- Inlamningsuppgift1/src/computer/Computer.java
+++ Inlamningsuppgift1/src/computer/Computer.java
@@ -1,7 +1,7 @@
 package computer;
 
 public class Computer {
-	public Computer(Memory memory)
-    public void load(Program program)
-    public void run()
+	public Computer(Memory memory);
+    public void load(Program program);
+    public void run();
 }
