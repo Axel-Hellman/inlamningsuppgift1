1e73b8e8618e29f99e3a67f824cf40cbb507a9cc
AxelHellman <axel.hellman@outlook.com>
2018-10-03 13:07:57 +0200

    Add skeleton for Memory.

--- src/computer/Memory.java
+++ src/computer/Memory.java
@@ -1,5 +1,14 @@
 package computer;
 
 public abstract class Memory {
+	
+	protected Word[] memory;
+	
+	public Word read(int index) {
+		return null;
+	}
 
+	public void write(Word word, int index) {
+		
+	}
 }
