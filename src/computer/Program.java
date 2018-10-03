337e8ce2fbdb9472c6794c0462a69712a25eca85
AxelHellman <axel.hellman@outlook.com>
2018-10-03 13:46:47 +0200

    Add skeleton to Program.

--- src/computer/Program.java
+++ src/computer/Program.java
@@ -1,5 +1,19 @@
 package computer;
 
+import java.util.ArrayList;
+
+import command.Command;
+
 public abstract class Program {
+
+	protected ArrayList<Command> commands;
+
+	public Command[] getCommands() {
+		return null;
+	}
+
+	public String toString() {
+		return null;
+	}
 
 }
