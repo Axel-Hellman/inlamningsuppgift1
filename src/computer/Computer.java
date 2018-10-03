7fb4b7410a09b6c3be2fdc0b58ceabcfd509fcad
AxelHellman <axel.hellman@outlook.com>
2018-10-03 11:20:03 +0200

    Add Computer class skeleton

--- src/computer/Computer.java
+++ src/computer/Computer.java
@@ -6,6 +6,10 @@ public class Computer {
 
 	private Memory memory;
 	private Command commands;
+	
+	public Computer (Memory memory) {
+		this.memory = memory;
+	}
 
 	public void load(Program program) {
 		
