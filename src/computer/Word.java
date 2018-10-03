718113f6cb808009ac358d3b32f18b88a88af87d
AxelHellman <axel.hellman@outlook.com>
2018-10-03 13:30:32 +0200

    Add Word code.

--- src/computer/Word.java
+++ src/computer/Word.java
@@ -1,5 +1,19 @@
 package computer;
 
-public abstract class Word {
+public abstract class Word implements Value {
+
+	// OBS! Konstinga signaturer i Equals och Copy?
+
+	public abstract Word getValue(Memory memory);
+
+	public abstract Word Add(Word w1, Word w2);
+
+	public abstract Word Mul(Word w1, Word w2);
+
+	public abstract Word Copy(Address fromAddress, Address toAddress);
+
+	public abstract boolean Equals(Word w1, Word w2);
+
+	public abstract String toString();
 
 }
