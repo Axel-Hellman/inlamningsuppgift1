1441ecf9a0144a8a5f6d0f6ee8906809d9e6b737
AxelHellman <axel.hellman@outlook.com>
2018-10-03 13:15:42 +0200

    Add skeleton for Address.

--- src/computer/Address.java
+++ src/computer/Address.java
@@ -1,5 +1,23 @@
 package computer;
 
-public class Address {
+public class Address implements Value {
+
+	private int index;
+
+	public Address(int index) {
+		this.index = index;
+	}
+
+	public Word getValue(Memory memory) {
+		return null;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public String toSting() {
+		return null;
+	}
 
 }
