198545ce6347d2c1acd1da1d6cfacc9487cbdcad
AxelHellman <axel.hellman@outlook.com>
2018-10-03 13:36:20 +0200

    Add skeleton for LongWord/ByteWord

--- src/computer/ByteWord.java
+++ src/computer/ByteWord.java
@@ -1,5 +1,54 @@
 package computer;
 
-public class ByteWord {
+												// DETTA KÄNNS FEL, RÄTTA PLS
+
+public class ByteWord extends Word {
+
+	private byte value;
+	
+	public ByteWord(int length) {
+	}
+
+	@Override
+	public Word getWord(Memory memory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Word getValue(Memory memory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Word Add(Word w1, Word w2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Word Mul(Word w1, Word w2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Word Copy(Address fromAddress, Address toAddress) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean Equals(Word w1, Word w2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/computer/LongWord.java
+++ src/computer/LongWord.java
@@ -1,5 +1,54 @@
 package computer;
 
-public class LongWord {
+												// DETTA KÄNNS FEL, RÄTTA PLS
+
+public class LongWord extends Word{
+	
+	private long value;
+	
+	public LongWord(int length) {
+	}
+
+	@Override
+	public Word getWord(Memory memory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Word getValue(Memory memory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Word Add(Word w1, Word w2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Word Mul(Word w1, Word w2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Word Copy(Address fromAddress, Address toAddress) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean Equals(Word w1, Word w2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
