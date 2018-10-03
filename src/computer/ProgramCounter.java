a24d48178513aa4e1d6b0c7d42bf92e21ffc678a
AxelHellman <axel.hellman@outlook.com>
2018-10-03 13:03:21 +0200

    Finish ProgramCounter

--- src/computer/ProgramCounter.java
+++ src/computer/ProgramCounter.java
@@ -2,4 +2,32 @@ package computer;
 
 public class ProgramCounter {
 
+	private int index;
+
+	public ProgramCounter() {
+		index = 0;
+	}
+
+	public int getIndex() {
+		return 0;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public void increase() {
+		index++;
+	}
+
+	public boolean isPositive() {
+
+		if (index >= 0) {
+			return true;
+		}
+
+		return false;
+
+	}
+
 }
