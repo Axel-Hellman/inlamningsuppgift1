4fd4148118a5bde79759546112aa3a51dfc84d22
JoakimRudberg <rudberg.joakim@gmail.com>
2018-10-03 14:35:49 +0200

    Modify Factorial

--- src/computer/Factorial.java
+++ src/computer/Factorial.java
@@ -1,5 +1,22 @@
 package computer;
 
-public class Factorial {
+import command.*;
+
+public class Factorial extends Program {
+	
+	public Factorial  (String value, WordFactory wf) {
+        Address n = new Address(0),
+                fac = new Address(1);
+        /**
+        Add(new Copy(wf.word(value), n));
+        Add(new Copy(wf.word("1"), fac));
+        Add(new JumpEq(6, n, wf.word("1")));
+        Add(new Mul(fac, n, fac));
+        Add(new Add(n, wf.word("-1"), n));
+        Add(new Jump(2));
+        Add(new Print(fac));
+        Add(new Halt());
+        */
+    }
 
 }
