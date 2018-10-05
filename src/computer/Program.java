a50f57b6832df51a264baf01772d086ca5444e3b
Treedill <johannes@tornblom.eu>
2018-10-05 10:17:53 +0200

    Program should be complete, might need to fix some later

--- src/computer/Program.java
+++ src/computer/Program.java
@@ -7,13 +7,26 @@ import command.Command;
 public abstract class Program {
 
 	protected ArrayList<Command> commands;
+	
+	protected Program() {
+		commands = new ArrayList<Command>();
+	}
 
 	public Command[] getCommands() {
-		return null;
+		Command[] comArray = new Command[0];
+		return commands.toArray(comArray);
 	}
 
 	public String toString() {
-		return null;
+		StringBuilder sb = new StringBuilder();
+		int index = 0;
+		
+		for(Command c: commands) {
+			sb.append(String.valueOf(index++) + " ");
+			sb.append(c.toString());
+			sb.append("\n");
+		}
+		return sb.toString();
 	}
 
 }
