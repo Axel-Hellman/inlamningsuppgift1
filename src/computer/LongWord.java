c85450daf7bc7d1bb716285e4e6a963b8460857d
Treedill <johannes@tornblom.eu>
2018-10-05 13:40:31 +0200

    Removed TO-DO-Stubs

--- src/computer/LongWord.java
+++ src/computer/LongWord.java
@@ -11,19 +11,16 @@ public class LongWord extends Word{
 
 	@Override
 	public Word add(Word w1, Word w2) {
-		// TODO Auto-generated method stub
 		return new LongWord(((LongWord)w1).value + ((LongWord)w2).value);
 	}
 
 	@Override
 	public Word mul(Word w1, Word w2) {
-		// TODO Auto-generated method stub
 		return new LongWord(((LongWord)w1).value * ((LongWord)w2).value);
 	}
 
 	@Override
 	public boolean equals(Word w1) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
