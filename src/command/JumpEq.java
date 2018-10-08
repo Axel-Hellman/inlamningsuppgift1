24f4e1d5d621393de6477c7cab446effebc56d88
Sandra <Sandra@Sandras>
2018-10-08 09:52:06 +0200

    implement operation in JumpEq.

--- src/command/JumpEq.java
+++ src/command/JumpEq.java
@@ -10,14 +10,16 @@ public class JumpEq extends JumpInstruction {
 	private Value equals1;
 	private Value equals2;
 
-	public JumpEq(int jumpTo, Value equals1, Value equals2) {
-		super(jumpTo);
+	public JumpEq(int index, Value equals1, Value equals2) {
+		super(index);
 		this.equals1 = equals1;
 		this.equals2 = equals2;
 	}
 	
 	public void operation(Memory memory, ProgramCounter counter) {
-
+		if (equals1.equals(equals2)){
+			counter.setIndex(index);
+		}
 	}
 
 	public String toString() {
