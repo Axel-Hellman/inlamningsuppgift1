618783b22a56650e60b71419c23269d475bb2839
JoakimRudberg <rudberg.joakim@gmail.com>
2018-10-09 15:03:23 +0200

    Implement CommentCell

--- src/model/CommentCell.java
+++ src/model/CommentCell.java
@@ -3,15 +3,23 @@ package model;
 import expr.Environment;
 
 public class CommentCell implements Cell {
+	private String string;
 	
 	public CommentCell(String cellString){
-		
+		this.string = cellString;
 	}
 	
 	@Override
 	public double value(Environment env) {
-		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public String getComment(){
+		return string;
+	}
+	
+	public String toString(Environment e){
+		return string;
 	}
 
 }
