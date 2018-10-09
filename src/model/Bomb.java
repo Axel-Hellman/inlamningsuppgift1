fab735371fa94ab658b9bd312f1abcf9bb3bf6f1
JoakimRudberg <rudberg.joakim@gmail.com>
2018-10-09 15:29:05 +0200

    Implement Bomb

--- src/model/Bomb.java
+++ src/model/Bomb.java
@@ -1,25 +1,23 @@
 package model;
 
 import expr.Environment;
+import util.XLException;
 
 public class Bomb implements Cell {
 
 	@Override
 	public double value(Environment env) { // BOOOOOOOOOM!!!
-		// TODO Auto-generated method stub 
-		return 0;
+		throw new XLException("KABOOM! (Bomb Error)");
 	}
 
 	@Override
 	public String getComment() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String toString(Environment env) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new XLException("KABOOM! (Bomb Error)");
 	}
 
 }
