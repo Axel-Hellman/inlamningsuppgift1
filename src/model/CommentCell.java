b7235072e285da276e960ce9b210334761a3cd22
JoakimRudberg <rudberg.joakim@gmail.com>
2018-10-09 14:58:11 +0200

    Implemented Factory

--- src/model/CommentCell.java
+++ src/model/CommentCell.java
@@ -3,7 +3,11 @@ package model;
 import expr.Environment;
 
 public class CommentCell implements Cell {
-
+	
+	public CommentCell(String cellString){
+		
+	}
+	
 	@Override
 	public double value(Environment env) {
 		// TODO Auto-generated method stub
--- src/model/ExprCell.java
+++ src/model/ExprCell.java
@@ -1,9 +1,13 @@
 package model;
 
 import expr.Environment;
+import expr.Expr;
 
 public class ExprCell implements Cell {
-
+	
+	public ExprCell (Expr exprCell){
+		
+	}
 	@Override
 	public double value(Environment env) {
 		// TODO Auto-generated method stub
